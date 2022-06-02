077774c587e002eeba800ba621903ec208c392c4
ahmet-ceng <65031185+ahmet-ceng@users.noreply.github.com>
2022-06-02 14:45:40 +0300

    Update main.py
    Finished Project

--- main.py
+++ main.py
@@ -25,12 +25,11 @@ data = {"TL_Balance": 100000, "Dollar_Balance": 0, "Euro_Balance": 0, "Gold_Bala
         "Total_Balance(TR)": 100000}
 db.child("Account").set(data)
 
-
 # Creating webdriver with given options
 options = Options()
 options.add_argument("--window-size=1920,1080")
 driver = webdriver.Chrome(options=options)
-driver.get("https://xn--dviz-5qa.com/")
+driver.get("https://www.döviz.com/")
 
 # Xpath for prices of Dollar, Euro and Gold
 dollarBuyPriceXpath = "/html/body/main/div/div/div/table/tbody/tr[1]/td[3]"
@@ -50,6 +49,7 @@ pricePaths = [dollarBuyPriceXpath, dollarSellPriceXpath, EuroBuyPriceXpath, Euro
 previousPrices = []
 currentPrices = []
 
+
 # Function to get all of the values from website with given Xpath's
 def get_prices():
     # Because we will keep updating prices, we need to clear the list before initialization
@@ -57,13 +57,63 @@ def get_prices():
     for i in pricePaths:
         currentPrices.append(driver.find_element(by=By.XPATH, value=i).text)
 
+
 # Getting previous prices (which is required only at first iteration)
 for i in pricePaths:
     previousPrices.append(driver.find_element(by=By.XPATH, value=i).text)
 
+
+def dollarAccountCheck():
+    if (currentPrices[0] < (previousPrices[0] - previousPrices[0] / 100)) and (balances["TL_Balance"] > 1000):
+        balances["TL_Balance"] = balances["TL_Balance"] - 10 * currentPrices[0]
+        balances["Dollar_Balance"] = balances["Dollar_Balance"] + 10
+        print("Bought 10 Dollar for: ", currentPrices[0], " each.")
+    if currentPrices[1] > (previousPrices[1] + previousPrices[1] / 200) and (balances["Dollar_Balance"] >= 5):
+        balances["TL_Balance"] = balances["TL_Balance"] + 5 * currentPrices[1]
+        balances["Dollar_Balance"] = balances["Dollar_Balance"] - 5
+        print("Sold 5 Dollar for: ", currentPrices[1], " each.")
+
+
+def euroAccountCheck():
+    if (currentPrices[2] < (previousPrices[2] - previousPrices[2] / 100)) and (balances["TL_Balance"] > 1000):
+        balances["TL_Balance"] = balances["TL_Balance"] - 10 * currentPrices[2]
+        balances["Euro_Balance"] = balances["Euro_Balance"] + 10
+        print("Bought 10 Euro for: ", currentPrices[2], " each.")
+    if currentPrices[3] > (previousPrices[3] + previousPrices[3] / 200) and (balances["Euro_Balance"] >= 5):
+        balances["TL_Balance"] = balances["TL_Balance"] + 5 * currentPrices[3]
+        balances["Euro_Balance"] = balances["Euro_Balance"] - 5
+        print("Sold 5 Euro for: ", currentPrices[3], " each.")
+
+
+def goldAccountCheck():
+    if (currentPrices[4] < (previousPrices[4] - previousPrices[4] / 100)) and (balances["TL_Balance"] > 1000):
+        balances["TL_Balance"] = balances["TL_Balance"] - 10 * currentPrices[4]
+        balances["Gold_Balance"] = balances["Gold_Balance"] + 10
+        print("Bought 10 Gold for: ", currentPrices[4], " each.")
+    if currentPrices[5] > (previousPrices[5] + previousPrices[5] / 200) and (balances["Gold_Balance"] >= 5):
+        balances["TL_Balance"] = balances["TL_Balance"] + 5 * currentPrices[5]
+        balances["Gold_Balance"] = balances["Gold_Balance"] - 5
+        print("Sold 5 Gold for: ", currentPrices[5], " each.")
+
+
+def calculateTotalBalance():
+    # Calculating Total TR balance with new exchange rates according bought or sold units
+    balances["Total_Balance(TR)"] = balances["TL_Balance"] + balances["Dollar_Balance"] * currentPrices[0] + balances[
+        "Euro_Balance"] * currentPrices[2] + balances["Gold_Balance"] * currentPrices[4]
+    # Update bank account (database)
+    db.child("Account").set(balances)
+    print(balances)
+
+
 # Main loop that checks prices every hour and buys or sells according to price changes.
 # Then updates the bank account (database) with current exchange prices and completed transactions
+# timeCheck is for keeping track of the number of loops
+timeCheck = 0
 while True:
+    timeCheck += 1
+    # Refresh the page before executing commands (comment this while testing)
+    # driver.refresh()
+
     # Get current exchange rates
     get_prices()
 
@@ -81,48 +131,23 @@ while True:
     }
 
     # Buy 10 Unit (Dollar, Euro or Gold) if buying price is %1 lower than the previous noted rate
-    # ...and the turkish lira account has more than 1000 TLs
+    # ...and the Turkish lira account has more than 1000 TLs
     # -----------------------------------------------------------------------------------------------
     # Sell 5 Unit (Dollar, Euro or Gold) if selling price is %0.5 higher than the previous noted rate
     # ...and the Unit account has more than or equal to 5 dollars
-    #-----------------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------------
     # Than update account balances according to current exchange rates and completed transactions
 
     # For Dollar Account
-    if (currentPrices[0] < (previousPrices[0] - previousPrices[0] / 100)) and (balances["TL_Balance"] > 1000):
-        balances["TL_Balance"] = balances["TL_Balance"] - 10 * currentPrices[0]
-        balances["Dollar_Balance"] = balances["Dollar_Balance"] + 10
-        print("bought 10 dollars")
-    if currentPrices[1] > (previousPrices[1] + previousPrices[1] / 200) and (balances["Dollar_Balance"] >= 5):
-        balances["TL_Balance"] = balances["TL_Balance"] + 5 * currentPrices[1]
-        balances["Dollar_Balance"] = balances["Dollar_Balance"] - 5
-        print(" sold 5 dollars")
-
+    dollarAccountCheck()
     # For Euro Account
-    if (currentPrices[2] < (previousPrices[2] - previousPrices[2] / 100)) and (balances["TL_Balance"] > 1000):
-        balances["TL_Balance"] = balances["TL_Balance"] - 10 * currentPrices[2]
-        balances["Euro_Balance"] = balances["Euro_Balance"] + 10
-        print(" bought 10 euros")
-    if currentPrices[3] > (previousPrices[3] + previousPrices[3] / 200) and (balances["Euro_Balance"] >= 5):
-        balances["TL_Balance"] = balances["TL_Balance"] + 5 * currentPrices[3]
-        balances["Euro_Balance"] = balances["Euro_Balance"] - 5
-        print(" sold 5 euros")
-
+    euroAccountCheck()
     # For Gold Account
-    if (currentPrices[4] < (previousPrices[4] - previousPrices[4] / 100)) and (balances["TL_Balance"] > 1000):
-        balances["TL_Balance"] = balances["TL_Balance"] - 10 * currentPrices[4]
-        balances["Gold_Balance"] = balances["Gold_Balance"] + 10
-        print(" bought 10 Gold")
-    if currentPrices[5] > (previousPrices[5] + previousPrices[5] / 200) and (balances["Gold_Balance"] >= 5):
-        balances["TL_Balance"] = balances["TL_Balance"] + 5 * currentPrices[5]
-        balances["Gold_Balance"] = balances["Gold_Balance"] - 5
-        print(" sold 5 Gold")
+    goldAccountCheck()
 
     # Calculating Total TR balance with new exchange rates according bought or sold units
-    balances["Total_Balance(TR)"] = balances["TL_Balance"] + balances["Dollar_Balance"] * currentPrices[0] + balances[
-        "Euro_Balance"] * currentPrices[2] + balances["Gold_Balance"] * currentPrices[4]
-    # Update bank account (database)
-    db.child("Account").set(balances)
+    # ... and update the bank account (database)
+    calculateTotalBalance()
 
     # Update previous price list
     previousPrices = currentPrices.copy()
@@ -130,5 +155,12 @@ while True:
     # Wait for 1 hour (3600 seconds)
     time.sleep(5)
 
-    # driver ile ilgili birkaç kod daha yazılabilir (herhangi bir hata ile karşılaşma
-    # ...durumunda çökmemesi için önlemler gibi)
+    # If loop has run 24 times
+    if timeCheck == 24:
+        break
+
+# If  balance is more than 100.000 TLs, than print out “YOU WIN!”, else print out “YOU’RE LOSER!”
+if balances["Total_Balance(TR)"] > 100000:
+    print("YOU WIN!")
+else:
+    print("YOU’RE LOSER!")
